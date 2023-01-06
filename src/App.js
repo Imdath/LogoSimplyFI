861b6a90cd50d5f71d0231226712f0a4a1c5cea1
Imdath <113598588+Imdath@users.noreply.github.com>
2023-01-06 13:14:15 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="app">
+      <div className="container">
+        <h1 className="title">
+          HTML<span class="and">&</span>CSS
+        </h1>
+        <p className="description">change and build websites</p>
+      </div>
+    </div>
+  );
+}
